229ed6d1d548785d0a4c8a51029a7fefb1d471b1
taiga1071101 <taigatukimi@gmail.com>
2025-02-22 12:27:02 +0900

    App修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
 
@@ -9,4 +10,6 @@ function App() {
   )
 }
 
-export default App
+//export default App
+export { App }
+
